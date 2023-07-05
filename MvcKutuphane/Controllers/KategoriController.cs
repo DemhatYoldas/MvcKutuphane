a888cf712aa314a265398d9b75badc300c7b840e
Demhat Yoldaş <64671581+DemhatYoldas@users.noreply.github.com>
2023-07-06 02:33:24 +0300

    Kategori

---
+++ MvcKutuphane/Controllers/KategoriController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcKutuphane.Models.Entity;
+
+namespace MvcKutuphane.Controllers
+{
+    public class KategoriController : Controller
+    {
+        DBKUTUPHANEEntities db=new DBKUTUPHANEEntities();
+       
+        // GET: Kategori
+        public ActionResult Index()
+        {
+            var degerler = db.TBLKATEGORI.ToList();
+            return View(degerler);
+        }
+    }
+}
---
+++ MvcKutuphane/Controllers/KitapVerController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcKutuphane.Controllers
+{
+    public class KitapVerController : Controller
+    {
+        // GET: KitapVer
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
