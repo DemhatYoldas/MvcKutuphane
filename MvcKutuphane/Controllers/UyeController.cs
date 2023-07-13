09eccbfcdd2bc08909300a0ccda5c0c05a4bdf74
Demhat Yoldaş <64671581+DemhatYoldas@users.noreply.github.com>
2023-07-13 15:45:24 +0300

    Uyeler

--- MvcKutuphane/Controllers/UyeController.cs
+++ MvcKutuphane/Controllers/UyeController.cs
@@ -3,15 +3,71 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MvcKutuphane.Models.Entity;
+using PagedList.Mvc;
+using PagedList;
 
 namespace MvcKutuphane.Controllers
 {
     public class UyeController : Controller
     {
         // GET: Uye
-        public ActionResult Index()
+        DBKUTUPHANEEntities db = new DBKUTUPHANEEntities();
+        public ActionResult Index(int sayfa=1)
+        {
+            var degerler = db.TBLUYELER.ToList().ToPagedList(sayfa,3);
+            return View(degerler);
+        }
+
+        [HttpGet]
+        public ActionResult UyeEkle()
         {
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult UyeEkle(TBLUYELER p) 
+        {
+            if (!ModelState.IsValid)
+            {
+                return View("UyeEkle");
+            }
+            else
+            {
+                db.TBLUYELER.Add(p);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+        }
+
+        public ActionResult UyeSil(int id )
+        {
+            var uye = db.TBLUYELER.Find(id);
+            db.TBLUYELER.Remove(uye);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult UyeGetir(int id)
+        {
+            var uye = db.TBLUYELER.Find(id);
+            return View("UyeGetir", uye);
+        }
+
+        public ActionResult UyeGuncelle(TBLUYELER p)
+        {
+            var uye = db.TBLUYELER.Find(p.ID);
+            uye.AD=p.AD;
+            uye.SOYADI=p.SOYADI;
+            uye.MAIL = p.MAIL;
+            uye.KULLANICIADI = p.KULLANICIADI;
+            uye.SIFRE=p.SIFRE;
+            uye.FOTOGRAF = p.FOTOGRAF;
+            uye.TELEFON = p.TELEFON;
+            uye.OKUL=p.OKUL;
+            
+            db.SaveChanges();
+            return RedirectToAction("Index");
         }
     }
 }
