5a0dcf397f1ac8e754bd4aed55f9cf0d9f6e5473
Demhat Yoldaş <64671581+DemhatYoldas@users.noreply.github.com>
2023-07-16 02:19:33 +0300

    Hareket Tablosu Ve İşlemler

--- MvcKutuphane/Controllers/İslemController.cs
+++ MvcKutuphane/Controllers/İslemController.cs
@@ -3,15 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MvcKutuphane.Models.Entity;
 
 namespace MvcKutuphane.Controllers
 {
     public class İslemController : Controller
     {
         // GET: İslem
+        DBKUTUPHANEEntities db=new DBKUTUPHANEEntities();
         public ActionResult Index()
         {
-            return View();
+            var degerler = db.TBLHAREKET.Where(x => x.ISLEMDURUM == true).ToList();
+            return View(degerler);
         }
     }
 }
