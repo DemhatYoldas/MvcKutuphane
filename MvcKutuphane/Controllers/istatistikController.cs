415f11c863a4772b4e9535d943d22a6f8be5e733
Demhat Yoldaş <64671581+DemhatYoldas@users.noreply.github.com>
2023-07-25 03:35:10 +0300

    istatistik kartlara veri çekilmesi

--- MvcKutuphane/Controllers/istatistikController.cs
+++ MvcKutuphane/Controllers/istatistikController.cs
@@ -13,6 +13,14 @@ namespace MvcKutuphane.Controllers
         DBKUTUPHANEEntities db=new DBKUTUPHANEEntities();
         public ActionResult Index()
         {
+            var uyesayi = db.TBLUYELER.Count();
+            var kitapsayi = db.TBLKITAP.Count();
+            var Odunckitap = db.TBLKITAP.Where(x=>x.DURUM==false).Count();
+            var Kasa = db.TBLCEZALAR.Sum(x=>x.PARA);
+            ViewBag.Dgruye = uyesayi;
+            ViewBag.Dgrkitap = kitapsayi;
+            ViewBag.Dgrodunc = Odunckitap;
+            ViewBag.Dgrkasa = Kasa;
             return View();
         }
 
