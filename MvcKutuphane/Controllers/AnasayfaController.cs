592b324a58165b78e954901d829e79cee50cb337
Demhat Yoldaş <64671581+DemhatYoldas@users.noreply.github.com>
2023-07-25 05:23:46 +0300

    galrei ve istatistiklerde yenileme

---
+++ MvcKutuphane/Controllers/AnasayfaController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcKutuphane.Models.Entity;
+
+namespace MvcKutuphane.Controllers
+{
+    public class AnasayfaController : Controller
+    {
+        // GET: Anasayfa
+        DBKUTUPHANEEntities db=new DBKUTUPHANEEntities();
+        public ActionResult Index()
+        {
+            var uyesayi = db.TBLUYELER.Count();
+            var kitapsayi = db.TBLKITAP.Count();
+            var Odunckitap = db.TBLKITAP.Where(x=>x.DURUM==false).Count();
+            var Kasa = db.TBLCEZALAR.Sum(x=>x.PARA);
+            ViewBag.Dgruye = uyesayi;
+            ViewBag.Dgrkitap = kitapsayi;
+            ViewBag.Dgrodunc = Odunckitap;
+            ViewBag.Dgrkasa = Kasa;
+            return View();
+        }
+
+    }
+}
--- MvcKutuphane/Controllers/istatistikController.cs
+++ MvcKutuphane/Controllers/istatistikController.cs
@@ -10,19 +10,10 @@ namespace MvcKutuphane.Controllers
     public class istatistikController : Controller
     {
         // GET: istatistik
-        DBKUTUPHANEEntities db=new DBKUTUPHANEEntities();
+        DBKUTUPHANEEntities db = new DBKUTUPHANEEntities();
         public ActionResult Index()
         {
-            var uyesayi = db.TBLUYELER.Count();
-            var kitapsayi = db.TBLKITAP.Count();
-            var Odunckitap = db.TBLKITAP.Where(x=>x.DURUM==false).Count();
-            var Kasa = db.TBLCEZALAR.Sum(x=>x.PARA);
-            ViewBag.Dgruye = uyesayi;
-            ViewBag.Dgrkitap = kitapsayi;
-            ViewBag.Dgrodunc = Odunckitap;
-            ViewBag.Dgrkasa = Kasa;
             return View();
         }
-
     }
 }
