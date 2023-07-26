5c57804bcd1c4916224da32a4e9c2923d0a54efd
Demhat Yoldaş <64671581+DemhatYoldas@users.noreply.github.com>
2023-07-27 00:48:53 +0300

    Login and Register

---
+++ MvcKutuphane/Controllers/KayitolController.cs
@@ -0,0 +1,32 @@
+﻿using MvcKutuphane.Models.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcKutuphane.Controllers
+{
+    public class KayitolController : Controller
+    {
+        // GET: Kayitol
+        DBKUTUPHANEEntities db = new DBKUTUPHANEEntities();
+        [HttpGet]
+        public ActionResult Kayit()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Kayit(TBLUYELER p)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View("Kayit");
+            }
+            db.TBLUYELER.Add(p);
+            db.SaveChanges();
+            return RedirectToAction("Girisyap", "Login");
+        }
+    }
+}
--- MvcKutuphane/Controllers/LoginController.cs
+++ MvcKutuphane/Controllers/LoginController.cs
@@ -3,12 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MvcKutuphane.Models.Entity;
 
 namespace MvcKutuphane.Controllers
 {
     public class LoginController : Controller
     {
         // GET: Login
+        DBKUTUPHANEEntities db=new DBKUTUPHANEEntities();
         public ActionResult Girisyap()
         {
             return View();
