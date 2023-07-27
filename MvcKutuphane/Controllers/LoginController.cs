f87e1f4df470f6cb698c97109701996c33d58bd4
Demhat Yoldaş <64671581+DemhatYoldas@users.noreply.github.com>
2023-07-27 05:59:45 +0300

    Login İşlemi

--- MvcKutuphane/Controllers/LoginController.cs
+++ MvcKutuphane/Controllers/LoginController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Security;
 using MvcKutuphane.Models.Entity;
 
 namespace MvcKutuphane.Controllers
@@ -14,6 +15,30 @@ namespace MvcKutuphane.Controllers
         public ActionResult Girisyap()
         {
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult Girisyap(TBLUYELER p)
+        {
+            var bilgiler = db.TBLUYELER.FirstOrDefault(x => x.MAIL == p.MAIL && x.SIFRE == p.SIFRE);
+            if (bilgiler !=null)
+            {
+                FormsAuthentication.SetAuthCookie(bilgiler.MAIL, false);
+                Session["id"] = bilgiler.ID.ToString();
+                Session["Resim"] = bilgiler.FOTOGRAF.ToString();
+                Session["Ad"] = bilgiler.AD.ToString();
+                Session["Soyad"] = bilgiler.SOYADI.ToString();
+                Session["Mail"] = bilgiler.MAIL.ToString();
+                Session["Kullaniciad"] = bilgiler.KULLANICIADI.ToString();
+                Session["Şifre"] = bilgiler.SIFRE.ToString();
+                Session["Okuladi"] = bilgiler.OKUL.ToString();
+                return RedirectToAction("Index","Panelim");
+
+            }
+            else
+            {
+                return View();
+            }
         }
     }
 }
